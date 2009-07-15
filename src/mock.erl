63e6ca6b7520eca8c17ad41e9fdb45978d9449fd
Brad Anderson <brad@sankatygroup.com>
2009-07-15 15:31:54 -0400

    mostly cosmetic changes, added include, ebin, timeout on mock shutdown, fixed some @spec

--- src/mock.erl
+++ src/mock.erl
@@ -1,10 +1,11 @@
+%%% -*-  erlang-indent-level:2  -*-
 %%%-------------------------------------------------------------------
 %%% File:      mock.erl
 %%% @author    Cliff Moon <> []
 %%% @copyright 2009 Cliff Moon
-%%% @doc  
+%%% @doc
 %%%
-%%% @end  
+%%% @end
 %%%
 %%% @since 2009-01-04 by Cliff Moon
 %%%-------------------------------------------------------------------
@@ -12,10 +13,10 @@
 -author('cliff@powerset.com').
 
 %% API
--export([mock/1, proxy_call/2, proxy_call/3, expects/4, expects/5, verify_and_stop/1, verify/1, stub_proxy_call/3, stop/1]).
+-export([mock/1, proxy_call/2, proxy_call/3, expects/4, expects/5,
+         verify_and_stop/1, verify/1, stub_proxy_call/3, stop/1]).
 
--include_lib("eunit/include/eunit.hrl").
--include("common.hrl").
+-include("../include/common.hrl").
 
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
@@ -27,9 +28,9 @@
 %% API
 %%====================================================================
 %%--------------------------------------------------------------------
-%% @spec mock(Module) -> {ok,Pid} | ignore | {error,Error}
+%% @spec mock(Module::atom()) -> {ok,Mock::record()} | ignore | {error,Error}
 %% @doc Starts the server
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 mock(Module) ->
   case gen_server:start_link({local, mod_to_name(Module)}, mock, Module, []) of
@@ -42,13 +43,13 @@ mock(Module) ->
 %% @end
 proxy_call(Module, Function) ->
   gen_server:call(mod_to_name(Module), {proxy_call, Function, {}}).
-  
+
 %% @spec proxy_call(Module::atom(), Function::atom(), Args::tuple()) -> term()
 %% @doc Proxies a call to the mock server for Module with arguments
 %% @end
 proxy_call(Module, Function, Args) ->
   gen_server:call(mod_to_name(Module), {proxy_call, Function, Args}).
-  
+
 stub_proxy_call(Module, Function, Args) ->
   RegName = list_to_atom(lists:concat([Module, "_", Function, "_stub"])),
   Ref = make_ref(),
@@ -57,32 +58,34 @@ stub_proxy_call(Module, Function, Args) ->
   receive
     {Ref, Answer} -> Answer
   end.
-  
+
+%% @spec expects(Module::atom(),
+%%               Function::atom(),
+%%               Args::function(),
+%%               Ret::function() | term() ) -> term()
+
+%%               Times:: {at_least, integer()} | never | {no_more_than, integer()} | integer()) -> term()
+
+%% @doc Sets the expectation that Function of Module will be called during a
+%% test with Args.   Args should be a fun predicate that will return true or
+%% false whether or not the argument list matches.  The argument list of the
+%% function is passed in as a tuple.  Ret is either a value to return or a fun
+%% of arity 2 to be evaluated in response to a proxied call.  The first argument
+%% is the actual args from the call, the second is the call count starting
+%% with 1.
 expects(Module, Function, Args, Ret) ->
   gen_server:call(mod_to_name(Module), {expects, Function, Args, Ret, 1}).
 
-%% @spec expects(Module::atom(), 
-%%               Function::atom(), 
-%%               Args::fun(ArgTuple),
-%%               Ret::fun(ArgTuple, Times) | term(),
-%%               Times:: {at_least, integer()} | infinite | {no_more_than, integer()} | integer()) -> term()
-%% @doc Sets the expectation that Function of Module will be called during a test with Args.
-%%      Args should be a fun predicate that will return true or false whether or not the argument
-%%      list matches.  The argument list of the function is passed in as a tuple.
-%%
-%%      Ret is either a value to return or a fun of arity 2 to be evaluated in response to a proxied
-%%      call.  The first argument is the actual args from the call, the second is the call count starting
-%%      with 1.
 expects(Module, Function, Args, Ret, Times) ->
   gen_server:call(mod_to_name(Module), {expects, Function, Args, Ret, Times}).
-  
-stub(Module, Function, Args, Ret) ->
-  gen_server:call(mod_to_name(Module), {stub, Function, Args, Ret}).
-  
+
+%% stub(Module, Function, Args, Ret) ->
+%%   gen_server:call(mod_to_name(Module), {stub, Function, Args, Ret}).
+
 verify_and_stop(Module) ->
   verify(Module),
   stop(Module).
-  
+
 verify(Module) ->
   ?assertEqual(ok, gen_server:call(mod_to_name(Module), verify)).
 
@@ -90,7 +93,7 @@ stop(Module) ->
   gen_server:cast(mod_to_name(Module), stop),
   timer:sleep(10).
 
-  
+
 %%====================================================================
 %% gen_server callbacks
 %%====================================================================
@@ -101,11 +104,11 @@ stop(Module) ->
 %%                         ignore               |
 %%                         {stop, Reason}
 %% @doc Initiates the server
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 init(Module) ->
   case code:get_object_code(Module) of
-    {Module, Bin, Filename} -> 
+    {Module, Bin, Filename} ->
       case replace_code(Module) of
         ok -> {ok, #state{module=Module,old_code={Module, Bin, Filename}}};
         {error, Reason} -> {stop, Reason}
@@ -114,7 +117,7 @@ init(Module) ->
   end.
 
 %%--------------------------------------------------------------------
-%% @spec 
+%% @spec
 %% handle_call(Request, From, State) -> {reply, Reply, State} |
 %%                                      {reply, Reply, State, Timeout} |
 %%                                      {noreply, State} |
@@ -122,21 +125,28 @@ init(Module) ->
 %%                                      {stop, Reason, Reply, State} |
 %%                                      {stop, Reason, State}
 %% @doc Handling call messages
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
-handle_call({proxy_call, Function, Args}, _From, State = #state{module=Mod,expectations=Expects}) ->
+handle_call({proxy_call, Function, Args}, _From,
+            State = #state{module=Mod,expectations=Expects}) ->
   case match_expectation(Function, Args, Expects) of
-    {matched, ReturnTerm, NewExpects} -> {reply, ReturnTerm, State#state{expectations=NewExpects}};
-    unmatched -> {stop, ?fmt("got unexpected call to ~p:~p", [Mod,Function])}
+    {matched, ReturnTerm, NewExpects} ->
+      {reply, ReturnTerm, State#state{expectations=NewExpects}};
+    unmatched ->
+      {stop, ?fmt("got unexpected call to ~p:~p", [Mod,Function])}
   end;
-  
-handle_call({expects, Function, Args, Ret, Times}, _From, State = #state{expectations=Expects}) ->
-  {reply, ok, State#state{expectations=add_expectation(Function, Args, Ret, Times, Expects)}};
 
-handle_call(verify, _From, State = #state{expectations=Expects,module=Mod}) ->
+handle_call({expects, Function, Args, Ret, Times}, _From,
+            State = #state{expectations=Expects}) ->
+  {reply, ok, State#state{
+          expectations=add_expectation(Function, Args, Ret, Times, Expects)}};
+
+handle_call(verify, _From,
+            State = #state{expectations=Expects,module=Mod}) ->
   ?infoFmt("verifying ~p~n", [Mod]),
   if
-    length(Expects) > 0 -> {reply, {mismatch, format_missing_expectations(Expects, Mod)}, State};
+    length(Expects) > 0 ->
+      {reply, {mismatch, format_missing_expectations(Expects, Mod)}, State};
     true -> {reply, ok, State}
   end.
 
@@ -145,9 +155,10 @@ handle_call(verify, _From, State = #state{expectations=Expects,module=Mod}) ->
 %%                                      {noreply, State, Timeout} |
 %%                                      {stop, Reason, State}
 %% @doc Handling cast messages
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 handle_cast(stop, State) ->
+  timer:sleep(10),
   {stop, normal, State}.
 
 %%--------------------------------------------------------------------
@@ -155,7 +166,7 @@ handle_cast(stop, State) ->
 %%                                       {noreply, State, Timeout} |
 %%                                       {stop, Reason, State}
 %% @doc Handling all non call/cast messages
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 handle_info(_Info, State) ->
     {noreply, State}.
@@ -166,17 +177,18 @@ handle_info(_Info, State) ->
 %% terminate. It should be the opposite of Module:init/1 and do any necessary
 %% cleaning up. When it returns, the gen_server terminates with Reason.
 %% The return value is ignored.
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 terminate(_Reason, #state{old_code={Module, Binary, Filename}}) ->
   code:purge(Module),
   code:delete(Module),
-  code:load_binary(Module, Filename, Binary).
+  code:load_binary(Module, Filename, Binary),
+  timer:sleep(10).
 
 %%--------------------------------------------------------------------
 %% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
 %% @doc Convert process state when code is changed
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
@@ -186,43 +198,49 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 format_missing_expectations(Expects, Mod) ->
   format_missing_expectations(Expects, Mod, []).
-  
+
 format_missing_expectations([], _, Msgs) ->
   lists:reverse(Msgs);
-  
-format_missing_expectations([{Function, Args, Ret, Times, Called}|Expects], Mod, Msgs) ->
-  Msgs1 = [?fmt("expected ~p:~p to be called ~p times but was called ~p", [Mod,Function,Times,Called])|Msgs],
+
+format_missing_expectations([{Function, _Args, _Ret, Times, Called}|Expects],
+                            Mod, Msgs) ->
+  Msgs1 = [?fmt("expected ~p:~p to be called ~p times but was called ~p",
+                [Mod,Function,Times,Called])|Msgs],
   format_missing_expectations(Expects, Mod, Msgs1).
 
 add_expectation(Function, Args, Ret, Times, Expects) ->
   Expects ++ [{Function, Args, Ret, Times, 0}].
-  
+
 match_expectation(Function, Args, Expectations) ->
   match_expectation(Function, Args, Expectations, []).
-  
+
 match_expectation(_Function, _Args, [], _Rest) ->
   unmatched;
-  
-match_expectation(Function, Args, [{Function, Matcher, Ret, MaxTimes, Invoked}|Expects], Rest) ->
+
+match_expectation(Function, Args, [{Function, Matcher, Ret, MaxTimes, Invoked} |
+                                   Expects], Rest) ->
   case Matcher(Args) of
     true ->
       ReturnTerm = prepare_return(Args, Ret, Invoked+1),
       if
-        Invoked + 1 >= MaxTimes -> {matched, ReturnTerm, lists:reverse(Rest) ++ Expects};
-        true -> {matched, ReturnTerm, lists:reverse(Rest) ++ [{Function, Matcher, Ret, MaxTimes, Invoked+1}] ++ Expects}
+        Invoked + 1 >= MaxTimes -> {matched, ReturnTerm, lists:reverse(Rest) ++
+                                    Expects};
+        true -> {matched, ReturnTerm, lists:reverse(Rest) ++
+                 [{Function, Matcher, Ret, MaxTimes, Invoked+1}] ++ Expects}
       end;
-    false -> match_expectation(Function, Args, Expects, [{Function,Matcher,Ret,MaxTimes,Invoked}|Rest])
+    false -> match_expectation(Function, Args, Expects,
+                               [{Function,Matcher,Ret,MaxTimes,Invoked}|Rest])
   end;
-  
+
 match_expectation(Function, Args, [Expect|Expects], Rest) ->
   match_expectation(Function, Args, Expects, [Expect|Rest]).
-  
+
 prepare_return(Args, Ret, Invoked) when is_function(Ret) ->
   Ret(Args, Invoked);
-  
-prepare_return(Args, Ret, Invoked) ->
+
+prepare_return(_Args, Ret, _Invoked) ->
   Ret.
-  
+
 replace_code(Module) ->
   Info = Module:module_info(),
   Exports = get_exports(Info),
@@ -233,7 +251,8 @@ replace_code(Module) ->
     {attribute,2,export,Exports}
   ] ++ NewFunctions,
   case compile:forms(Forms, [binary]) of
-    {ok, Module, Binary} -> case code:load_binary(Module, atom_to_list(Module) ++ ".erl", Binary) of
+    {ok, Module, Binary} -> case code:load_binary(Module, atom_to_list(Module)
+                                                  ++ ".erl", Binary) of
       {module, Module} -> ok;
       {error, Reason} -> {error, Reason}
     end;
@@ -244,43 +263,48 @@ replace_code(Module) ->
 unload_code(Module) ->
   code:purge(Module),
   code:delete(Module).
-  
+
 get_exports(Info) ->
   get_exports(Info, []).
-  
+
 get_exports(Info, Acc) ->
   case lists:keytake(exports, 1, Info) of
     {value, {exports, Exports}, ModInfo} ->
-      get_exports(ModInfo, Acc ++ lists:filter(fun({module_info, _}) -> false; (_) -> true end, Exports));
+      get_exports(ModInfo, Acc ++ lists:filter(fun({module_info, _}) ->
+                                                   false;
+                                                  (_) -> true end, Exports));
     _ -> Acc
   end.
-  
-stub_function_loop(Fun) ->
-  receive
-    {Ref, Pid, Args} ->
-      ?debugFmt("received {~p,~p,~p}", [Ref, Pid, Args]),
-      Ret = (catch Fun(Args) ),
-      ?debugFmt("sending {~p,~p}", [Ref,Ret]),
-      Pid ! {Ref, Ret},
-      stub_function_loop(Fun)
-  end.
+
+%% stub_function_loop(Fun) ->
+%%   receive
+%%     {Ref, Pid, Args} ->
+%%       ?debugFmt("received {~p,~p,~p}", [Ref, Pid, Args]),
+%%       Ret = (catch Fun(Args) ),
+%%       ?debugFmt("sending {~p,~p}", [Ref,Ret]),
+%%       Pid ! {Ref, Ret},
+%%       stub_function_loop(Fun)
+%%   end.
 
 % Function -> {function, Lineno, Name, Arity, [Clauses]}
 % Clause -> {clause, Lineno, [Variables], [Guards], [Expressions]}
 % Variable -> {var, Line, Name}
-% 
+%
 generate_functions(Module, Exports) ->
   generate_functions(Module, Exports, []).
-  
+
 generate_functions(_Module, [], FunctionForms) ->
   lists:reverse(FunctionForms);
-  
+
 generate_functions(Module, [{Name,Arity}|Exports], FunctionForms) ->
-  generate_functions(Module, Exports, [generate_function(Module, Name, Arity)|FunctionForms]).
-  
+  generate_functions(Module, Exports,
+                     [generate_function(Module, Name, Arity)|FunctionForms]).
+
 generate_function(Module, Name, Arity) ->
-  {function, 1, Name, Arity, [{clause, 1, generate_variables(Arity), [], generate_expression(mock, proxy_call, Module, Name, Arity)}]}.
-  
+  {function, 1, Name, Arity, [{clause, 1, generate_variables(Arity), [],
+                               generate_expression(mock, proxy_call, Module,
+                                                   Name, Arity)}]}.
+
 generate_variables(0) -> [];
 generate_variables(Arity) ->
   lists:map(fun(N) ->
@@ -290,28 +314,22 @@ generate_variables(Arity) ->
 generate_expression(M, F, Module, Name, 0) ->
   [{call,1,{remote,1,{atom,1,M},{atom,1,F}}, [{atom,1,Module}, {atom,1,Name}]}];
 generate_expression(M, F, Module, Name, Arity) ->
-  [{call,1,{remote,1,{atom,1,M},{atom,1,F}}, [{atom,1,Module}, {atom,1,Name}, {tuple,1,lists:map(fun(N) ->
+  [{call,1,{remote,1,{atom,1,M},{atom,1,F}}, [{atom,1,Module}, {atom,1,Name},
+                                              {tuple,1,lists:map(fun(N) ->
       {var, 1, list_to_atom(lists:concat(['Arg', N]))}
     end, lists:seq(1, Arity))}]}].
 
 mod_to_name(Module) ->
   list_to_atom(lists:concat([mock_, Module])).
-  
-replace_function(FF, Forms) ->
-  replace_function(FF, Forms, []).
-  
-replace_function(FF, [], Ret) ->
-  [FF|lists:reverse(Ret)];
-  
-replace_function({function,_,Name,Arity,Clauses}, [{function,Line,Name,Arity,_}|Forms], Ret) ->
-  lists:reverse(Ret) ++ [{function,Line,Name,Arity,Clauses}|Forms];
-  
-replace_function(FF, [FD|Forms], Ret) ->
-  replace_function(FF, Forms, [FD|Ret]).
-  
-  
-  
-  
-  
-  
-  
+
+%% replace_function(FF, Forms) ->
+%%   replace_function(FF, Forms, []).
+
+%% replace_function(FF, [], Ret) ->
+%%   [FF|lists:reverse(Ret)];
+
+%% replace_function({function,_,Name,Arity,Clauses}, [{function,Line,Name,Arity,_}|Forms], Ret) ->
+%%   lists:reverse(Ret) ++ [{function,Line,Name,Arity,Clauses}|Forms];
+
+%% replace_function(FF, [FD|Forms], Ret) ->
+%%   replace_function(FF, Forms, [FD|Ret]).
--- src/stub.erl
+++ src/stub.erl
@@ -2,9 +2,9 @@
 %%% File:      stub.erl
 %%% @author    Cliff Moon <> []
 %%% @copyright 2009 Cliff Moon
-%%% @doc  
+%%% @doc
 %%%
-%%% @end  
+%%% @end
 %%%
 %%% @since 2009-05-10 by Cliff Moon
 %%%-------------------------------------------------------------------
@@ -20,24 +20,21 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
 
--include_lib("eunit/include/eunit.hrl").
--include("common.hrl").
+-include("../include/common.hrl").
 
 -record(state, {old_code, module, stub, times}).
 
 %%====================================================================
 %% API
 %%====================================================================
-%%--------------------------------------------------------------------
-%% @doc Starts the server
-%% @end 
-%%--------------------------------------------------------------------
+
 stub(Module, Function, Fun) ->
   stub(Module, Function, Fun, 1).
 
 stub(Module, Function, Fun, Times) when is_function(Fun) ->
-  gen_server:start({local, name(Module, Function)}, ?MODULE, [Module, Function, Fun, Times], []).
-  
+  gen_server:start({local, name(Module, Function)}, ?MODULE,
+                   [Module, Function, Fun, Times], []).
+
 proxy_call(_, Name, Args) ->
   {Times, Reply} = gen_server:call(Name, {proxy_call, Args}),
   if
@@ -56,19 +53,20 @@ proxy_call(_, Name, Args) ->
 %%                         ignore               |
 %%                         {stop, Reason}
 %% @doc Initiates the server
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 init([Module, Function, Fun, Times]) ->
   case code:get_object_code(Module) of
     {Module, Bin, Filename} ->
       ?debugMsg("stubbing"),
       stub_function(Module, Function, arity(Fun)),
-      {ok, #state{module=Module,old_code={Module,Bin,Filename},times=Times,stub=Fun}};
+      {ok, #state{module=Module,old_code={Module,Bin,Filename},times=Times,
+                  stub=Fun}};
     error -> {stop, ?fmt("Could not get object code for module ~p", [Module])}
   end.
 
 %%--------------------------------------------------------------------
-%% @spec 
+%% @spec
 %% handle_call(Request, From, State) -> {reply, Reply, State} |
 %%                                      {reply, Reply, State, Timeout} |
 %%                                      {noreply, State} |
@@ -76,7 +74,7 @@ init([Module, Function, Fun, Times]) ->
 %%                                      {stop, Reason, Reply, State} |
 %%                                      {stop, Reason, State}
 %% @doc Handling call messages
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 handle_call({proxy_call, Args}, _From, State = #state{stub=Fun, times=Times}) ->
   Reply = apply(Fun, tuple_to_list(Args)),
@@ -87,9 +85,10 @@ handle_call({proxy_call, Args}, _From, State = #state{stub=Fun, times=Times}) ->
 %%                                      {noreply, State, Timeout} |
 %%                                      {stop, Reason, State}
 %% @doc Handling cast messages
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 handle_cast(stop, State) ->
+  sleep:timer(10),
   {stop, normal, State}.
 
 %%--------------------------------------------------------------------
@@ -97,7 +96,7 @@ handle_cast(stop, State) ->
 %%                                       {noreply, State, Timeout} |
 %%                                       {stop, Reason, State}
 %% @doc Handling all non call/cast messages
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 handle_info(_Info, State) ->
   {noreply, State}.
@@ -108,15 +107,15 @@ handle_info(_Info, State) ->
 %% terminate. It should be the opposite of Module:init/1 and do any necessary
 %% cleaning up. When it returns, the gen_server terminates with Reason.
 %% The return value is ignored.
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
-terminate(_Reason, #state{old_code={Module,Bin,Filename}}) ->
+terminate(_Reason, #state{old_code={_Module,_Bin,_Filename}}) ->
   ok.
 
 %%--------------------------------------------------------------------
 %% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
 %% @doc Convert process state when code is changed
-%% @end 
+%% @end
 %%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
@@ -126,14 +125,16 @@ code_change(_OldVsn, State, _Extra) ->
 %%--------------------------------------------------------------------
 name(Module, Function) ->
   list_to_atom(lists:concat([Module, Function, "stub"])).
-  
+
 stub_function(Module, Function, Arity) ->
   {_, Bin, _} = code:get_object_code(Module),
-  {ok, {Module,[{abstract_code,{raw_abstract_v1,Forms}}]}} = beam_lib:chunks(Bin, [abstract_code]),
+  {ok, {Module,[{abstract_code,{raw_abstract_v1,Forms}}]}} =
+        beam_lib:chunks(Bin, [abstract_code]),
   ?debugMsg("replacing function"),
   StubbedForms = replace_function(Module, Function, Arity, Forms),
   case compile:forms(StubbedForms, [binary]) of
-    {ok, Module, Binary} -> code:load_binary(Module, atom_to_list(Module) ++ ".erl", Binary);
+    {ok, Module, Binary} -> code:load_binary(Module, atom_to_list(Module) ++
+                                             ".erl", Binary);
     Other -> Other
   end.
 
@@ -143,16 +144,18 @@ arity(Fun) when is_function(Fun) ->
 
 replace_function(Module, Function, Arity, Forms) ->
   replace_function(Module, Function, Arity, Forms, []).
-  
-replace_function(Module, Function, Arity, [], Acc) ->
+
+replace_function(_Module, _Function, _Arity, [], Acc) ->
   lists:reverse(Acc);
-replace_function(Module, Function, Arity, [{function, Line, Function, Arity, _Clauses}|Forms], Acc) ->
+replace_function(Module, Function, Arity, [{function, Line, Function, Arity,
+                                            _Clauses}|Forms], Acc) ->
   lists:reverse(Acc) ++ [{function, Line, Function, Arity, [
     {clause,
       Line,
       generate_variables(Arity),
       [],
-      generate_expression(stub,proxy_call,Module,name(Module,Function),Arity)}]}] ++ Forms;
+      generate_expression(stub,proxy_call,Module,name(Module,Function),Arity)}]}]
+        ++ Forms;
 replace_function(Module, Function, Arity, [Form|Forms], Acc) ->
   replace_function(Module, Function, Arity, Forms, [Form|Acc]).
 
@@ -161,10 +164,11 @@ generate_variables(Arity) ->
   lists:map(fun(N) ->
       {var, 1, list_to_atom(lists:concat(['Arg', N]))}
     end, lists:seq(1, Arity)).
-    
+
 generate_expression(M, F, Module, Name, 0) ->
   [{call,1,{remote,1,{atom,1,M},{atom,1,F}}, [{atom,1,Module}, {atom,1,Name}]}];
 generate_expression(M, F, Module, Name, Arity) ->
-  [{call,1,{remote,1,{atom,1,M},{atom,1,F}}, [{atom,1,Module}, {atom,1,Name}, {tuple,1,lists:map(fun(N) ->
+  [{call,1,{remote,1,{atom,1,M},{atom,1,F}}, [{atom,1,Module}, {atom,1,Name},
+                                              {tuple,1,lists:map(fun(N) ->
       {var, 1, list_to_atom(lists:concat(['Arg', N]))}
     end, lists:seq(1, Arity))}]}].
